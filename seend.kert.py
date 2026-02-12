5ca47db04f2f4010bc97631694b66c752a22f8be
kert-one <142112390+douglaskert@users.noreply.github.com>
2026-02-12 10:10:52 -0300

    Update seend.kert.py

--- seend.kert.py
+++ seend.kert.py
@@ -249,7 +249,7 @@ def chain_chunk_api():
 
 # --- Classe Blockchain ---
 class Blockchain:
-    ADJUST_INTERVAL = 7200 #Blocos para recalcular dificuldade
+    ADJUST_INTERVAL = 10
     TARGET_TIME = 600 # Tempo alvo entre blocos em segundos (10 minutos)
     TARGET_WINDOW = ADJUST_INTERVAL * TARGET_TIME
 
